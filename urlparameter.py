dc2a5eac8f613cc849eec4b4f84a6a5607059e7b
VTYSPROJECT <116209979+VTYSPROJECT@users.noreply.github.com>
2022-11-16 11:10:26 +0300

    Url Collection

---
+++ urlparameter.py
@@ -0,0 +1,147 @@
+import requests
+from googletrans import Translator
+import psycopg2
+from lxml import html
+import lxml
+from requests_html import HTML,HTMLSession
+from bs4 import BeautifulSoup
+conn = psycopg2.connect(
+   database="VTYS", user='postgres', password='ONURonur44', host='127.0.0.1', port= '7956'
+)
+print("naber")
+class translate:
+    sentence=""
+    def cevir(self,sentence,dest):
+        translator=Translator()
+        self.sentence=sentence
+        trans=translator.translate(sentence,dest=dest).text
+        return trans
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu'}
+
+urls=list()
+for i in LANGUAGES.keys():
+    country_code = i
+    country = LANGUAGES[i]
+    translator = Translator()
+    session = HTMLSession()
+    search_param = translator.translate("maden kazaları", country_code)
+    r = session.get('https://www.google.com/search?q={}&hl={}'.format(search_param, country_code))
+    soup = BeautifulSoup(r.text, "lxml")
+    quotes = soup.find_all('div', {'class': 'yuRUbf'})
+    cursor = conn.cursor()
+    for i in quotes:
+        a = str(i)
+        if (a.split('"')[9].startswith("http")):
+            script = '''INSERT INTO public.urls(url,ulke) 
+                        VALUES ({},{});'''.format(a.split('"')[9], country_code)
+            cursor.execute(script)
+        else:
+            pass
+    conn.commit()
